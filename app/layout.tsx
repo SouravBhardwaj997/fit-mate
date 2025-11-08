e5dd7cda0f00aee38602bda4bb10087c5d29b509
Sourav <souravbhardwaj997@gmail.com>
2025-11-08 12:57:34 +0530

    implement layout structure with Navbar and Footer; add UserPrograms and TerminalOverlay components; update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,15 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <Navbar />
+          {/* GRID BACKGROUND */}
+          <div className="fixed inset-0 -z-1">
+            <div className="absolute inset-0 bg-linear-to-b from-background via-background to-background"></div>
+            <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-size-[20px_20px]"></div>
+          </div>
+
+          <main className="pt-24 grow">{children}</main>
+          <Footer />
         </body>
       </html>
     </ConvexClerkProvider>

ae13e7113deb0599073982f74cd2f9328ef9e9c7
Sourav <souravbhardwaj997@gmail.com>
2025-11-11 22:27:20 +0530

    Implement profile page with user plans and fitness details; add corner elements and no fitness plan component; integrate Radix UI accordion and tabs for better UI structure; update package dependencies.

---
+++ components/Profile/index.tsx
@@ -0,0 +1,5 @@
+import NoFitnessPlan from "./NoFitnessPlan";
+import ProfileHeader from "./ProfileHeader";
+import CornerElements from "./CornerElements";
+
+export { ProfileHeader, NoFitnessPlan, CornerElements };
